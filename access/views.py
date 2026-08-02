385f9b59600692804b8cf753d1e04427e0749349
Johnathan G. de Oliveira. <john.2801.oliver@gmail.com>
2026-08-02 13:40:34 -0300

    feat(access): simple access form validation

--- access/views.py
+++ access/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import redirect, render
 
 HARDCODE_USERS = [
     ('foo', 'bar'),
     ('bar', 'baz'),
 ]
 
+def access_error_description(error_name):
+    if error_name == 'invalid':
+        return 'Invalid username or password'
+    return ''
+
 def index(request):
-    context = {'name': 'Robien'}
-    template = loader.get_template('access/index.html')
-    return HttpResponse(template.render(context, request))
+    error_name = request.GET.get('error', '')
+    error_desc = access_error_description(error_name)
+
+    context = {'error': error_desc}
+    return render(request, "access/index.html", context)
 
 def enter(request):
     username = request.POST['username']
@@ -18,4 +25,5 @@ def enter(request):
     for (it_username, it_password) in HARDCODE_USERS:
         if it_username == username and it_password == password:
             return HttpResponse(200)
-    return HttpResponse(400)
+
+    return redirect(f'/access?error=invalid')
