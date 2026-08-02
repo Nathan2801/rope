da895f8471f4634f41c3c5b7ef7b5241749248de
Johnathan G. de Oliveira. <john.2801.oliver@gmail.com>
2026-08-02 13:01:03 -0300

    feat(access): naivy tests

--- access/views.py
+++ access/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
 
+HARDCODE_USERS = [
+    ('foo', 'bar'),
+    ('bar', 'baz'),
+]
+
 def index(request):
     context = {'name': 'Robien'}
     template = loader.get_template('access/index.html')
     return HttpResponse(template.render(context, request))
+
+def enter(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    for (it_username, it_password) in HARDCODE_USERS:
+        if it_username == username and it_password == password:
+            return HttpResponse(200)
+    return HttpResponse(400)
