bd3f52d855406d8d6fcb1d8acf3c01002b8627fc
Johnathan G. de Oliveira. <john.2801.oliver@gmail.com>
2026-08-03 12:18:31 -0300

    feat(dashboard): add dashboard page

--- rope/urls.py
+++ rope/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('access/', include('access.urls'))
+    path('', include('dashboard.urls')),
+    path('access/', include('access.urls')),
 ]
