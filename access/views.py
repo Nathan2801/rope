1a028369deca5edcdb535ced36fe85960e0e00da
Johnathan G. de Oliveira. <john.2801.oliver@gmail.com>
2026-08-03 12:46:26 -0300

    feat(access): add register page

--- access/views.py
+++ access/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 HARDCODE_USERS = [
     ('foo', 'bar'),
     ('bar', 'baz'),
 ]
 
-def access_error_description(error_name):
-    if error_name == 'invalid':
+def access_error_description(errid):
+    if errid == 'invalid':
         return 'Invalid username or password'
     return ''
 
 def index(request):
-    error_name = request.GET.get('error', '')
-    error_desc = access_error_description(error_name)
+    errid = request.GET.get('error', '')
+    error = access_error_description(errid)
 
-    context = {'error': error_desc}
+    context = {'error': error}
     return render(request, "access/index.html", context)
 
 def enter(request):
@@ -24,6 +25,26 @@ def enter(request):
 
     for (it_username, it_password) in HARDCODE_USERS:
         if it_username == username and it_password == password:
-            return HttpResponse(200)
+            return redirect('/')
 
     return redirect(f'/access?error=invalid')
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST.get('email', '')
+        if email == '':
+            return redirect('/access/register?error=invalid')
+
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == '' or password == '':
+            return redirect('/access/register?error=invalid')
+
+        # user = User.objects.create_user(username, email, password)
+        return redirect('/')
+
+    errid = request.GET.get('error', '')
+    error = access_error_description(errid)
+
+    context = {'error': error}
+    return render(request, 'access/register.html', context)
